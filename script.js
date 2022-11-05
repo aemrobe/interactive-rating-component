bf5ad875dea7cd3c15bf2f07fd145e6093982b03
aemro bekalu <aemrobekalu083@gmail.com>
2022-11-05 17:05:59 +0300

    making both the rating card and the thankyou card

---
+++ script.js
@@ -0,0 +1,39 @@
+const rating = document.querySelectorAll(".rating");
+const submitButton = document.querySelector(".submit-btn");
+const ratingCard = document.querySelector(".card");
+const thankyouCard = document.querySelector(".thankyou-card");
+const ratingNumber = document.querySelector(".rate");
+//function
+//this will change the background of the rating buttons when they rating buttons are cliked
+const addRating = function (ratingValue) {
+  rating[ratingValue].classList.toggle("click");
+
+  for (let i = 0; i < rating.length; i++) {
+    if (i !== ratingValue) {
+      rating[i].classList.remove("click");
+    }
+  }
+};
+
+//function
+//the rate buttons calling the addRating functions when they are clicked
+const callingAddRatingFunction = function (ratingValue) {
+  rating[ratingValue].addEventListener("click", function () {
+    addRating(ratingValue);
+  });
+};
+
+//adding the CallingAddRatingfunctions for all of the buttons available
+for (let i = 0; i < rating.length; i++) {
+  callingAddRatingFunction(i);
+}
+
+submitButton.addEventListener("click", function () {
+  for (let i = 0; i < rating.length; i++) {
+    if (rating[i].classList.contains("click")) {
+      ratingCard.classList.add("hidden");
+      ratingNumber.textContent = i + 1;
+      thankyouCard.classList.remove("hidden");
+    }
+  }
+});
